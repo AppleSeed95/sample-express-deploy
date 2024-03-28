1f72a74b3a31cffb77c58128c5593d25a42dd30c
AppleSeed <everlastingfriendship194@gmail.com>
2024-03-28 18:03:47 +0900

    fix:build

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Express on Vercel");
+// Serve static files from the public directory
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the static HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(5000, () => {
-    console.log("Running on port 5000.");
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-// Export the Express API
 module.exports = app;
